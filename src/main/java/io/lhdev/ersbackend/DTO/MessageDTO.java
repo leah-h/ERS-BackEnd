d3af3425246c09a575dcb978514567d4c4183039
Leah H <harrichl@gmail.com>
2021-04-26 17:57:18 -0400

    modify gitignore

--- src/main/java/io/lhdev/ersbackend/DTO/MessageDTO.java
+++ src/main/java/io/lhdev/ersbackend/DTO/MessageDTO.java
@@ -1,18 +1,2 @@
-package io.lhdev.ersbackend.DTO;
-
-public class MessageDTO {
-
-    private String message;
-
-    public MessageDTO() {
-        super();
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+package io.lhdev.ersbackend.DTO;public class MessageDTO {
 }
